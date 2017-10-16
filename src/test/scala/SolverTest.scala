6bd915d467fb763af07b45493234ab02a6fc7968
Ivan Obeso <ivan.obeso1@gmail.com>
2017-10-17 00:16:39 +0100

    added a couple of tests

--- src/test/scala/SolverTest.scala
+++ src/test/scala/SolverTest.scala
@@ -72,6 +72,10 @@ class SolverTest extends FunSuite {
     val preferences = List( List( (1,'M),(3,'G),(5,'G) ), List((2,'G), (3,'M), (4,'G)),
       List((5,'M)) )
     assert(solve(preferences, 5) == Some(List((1,'G), (2,'G), (3,'G), (4,'G), (5,'M))))
+    val impossibleSolution = List( List((1,'G)), List((1,'M)))
+    assert(solve(impossibleSolution, 1) == None)
+    val preferences2 = List( List((1,'G),(2,'M)), List((1,'M)))
+    assert(solve(preferences2, 2) == Some(List( (1,'M), (2,'M) )))
   }
 
   test("test of fillMissingColors(...)"){
