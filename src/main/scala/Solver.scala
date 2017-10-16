7f3e20e1905d4c7c6a2ee1692df09c225cbe0d0b
Ivan Obeso <ivan.obeso1@gmail.com>
2017-10-16 15:07:46 +0100

    fixed bug in matrix simplifier

--- src/main/scala/Solver.scala
+++ src/main/scala/Solver.scala
@@ -5,18 +5,37 @@ object Solver {
   type Preferences = List[Color]
   type UsersPreferences = List[Preferences]
 
+  /**
+    * Checks if there is any problem on the list of preferences for clients
+    * who has only one preference. If there are 2 of them who want the same
+    * color in different taste, then there is an inconsistency and returns
+    * false.
+    */
   def isPossible(singlePrefs: Preferences):Boolean = {
-    singlePrefs.distinct == singlePrefs.length
+    singlePrefs.groupBy(_._1).exists(x => x._2.length>1) == false
+  }
+
+  def removePreferences(pref: UsersPreferences, singlePrefs: Preferences): UsersPreferences = {
+    val optionsTaken = singlePrefs.map(x=>x._1)
+    val removed = pref.map(x => x.filter(y => optionsTaken.contains(y._1)==false ))
+    removed.filter(x => !x.isEmpty)
   }
 
   def simplifyMatrix(pref: UsersPreferences): Option[(Preferences,UsersPreferences)] = {
-    pref.find(_.length==1) match {
-      case Some(x) => if(!isPossible(x)) None else None
-      case None => None //Some(pref)
+    pref.filter(_.length==1).flatten match {
+      case Nil => Some(Nil,pref)
+      case xs => {
+        if(!isPossible(xs)) None else {
+          val simplifiedPref = removePreferences(pref, xs)
+          val a = simplifyMatrix(simplifiedPref)
+          a match {
+            case Some(y) => Some((xs:::y._1, y._2))
+            case None => None
+          }
+        }
+      }
     }
 
-      //else
-        //single :: simplifyMatrix(pref without single)
 
   }
 }
--- src/test/scala/SolverTest.scala
+++ src/test/scala/SolverTest.scala
@@ -10,5 +10,25 @@ class SolverTest extends FunSuite {
   test("test of isPossible(...) method"){
     val pref = List((1,'M),(2,'G),(3,'M))
     assert(isPossible(pref)==true)
+    val pref2 = List((1,'M),(1,'G),(3,'M))
+    assert(isPossible(pref2)==false)
+  }
+
+  test("test of removePreferences(...) method"){
+    val userPrefs = List( List((1,'M),(2,'M)), List((1,'G)), List((3,'M),(2,'G)) )
+    val singlePrefs = List( (1,'M), (1,'G), (2,'G) )
+    assert(removePreferences(userPrefs, singlePrefs) == List(List((3,'M))))
+
+    val userPrefs2 = List( List((1,'M),(2,'M)), List((3,'M),(2,'G)) )
+    val singlePrefs2 = List( (1,'G), (2,'G) )
+    assert(removePreferences(userPrefs2, singlePrefs2) == List(List((3,'M))))
+  }
+
+  test("test of simplifyMatrix(...)"){
+    val pref1 = List(List((1,'G),(2,'G),(3,'M)))
+    assert(simplifyMatrix(pref1) == Some((List(), List(List((1,'G),(2,'G),(3,'M))))) )
+
+    val pref2 = List(List((1,'M)), List((2,'M)), List((1,'G),(2,'G),(3,'M)))
+    assert(simplifyMatrix(pref2) == Some((List((1,'M),(2,'M),(3,'M)), List())) )
   }
 }
