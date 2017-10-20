54c90354d4a1d64edaee32f5b67e73a2599b951f
Ivan Obeso <ivan.obeso1@gmail.com>
2017-10-20 23:59:05 +0100

    added documentation

--- src/main/scala/Solver.scala
+++ src/main/scala/Solver.scala
@@ -6,27 +6,48 @@ object Solver {
   type UsersPreferences = List[Preferences]
 
   /**
-    * Checks if there is any problem on the list of preferences for clients
-    * who has only one preference. If there are 2 of them who want the same
-    * color in different taste, then there is an inconsistency and returns
-    * false.
+    * Checks if there is any conflict in the list of single preferences.
+    * Single preferences are the preferences from the customers who only
+    * have one of them, so they will always be part of the final solution
+    * (in case that there is a solution).
+    *
+    * If there are 2 customers who want the same color in different taste,
+    * then there is an inconsistency and returns false.
     */
-  def isPossible(singlePrefs: Preferences):Boolean = {
+  def existsSolution(singlePrefs: Preferences):Boolean = {
     singlePrefs.groupBy(_._1).map(x => x._2.map(y => y._2).distinct).exists(_.length!=1) == false
   }
 
-  def removePreferences(pref: UsersPreferences, singlePrefs: Preferences): UsersPreferences = {
+  /**
+    * This function removes the single preferences from the matrix of preferences of all the customers.
+    * For instance, if we have 2 customers one of them with (1 M) and the other with ((1 G), (2 M)), we
+    * can simplify the matrix removing (1 M) and (1 G), because we know (1 M) will be part of the
+    * solution so (1 G) is impossible to be selected. It returns the customer preferences
+    * without those single preferences nor the preferences that can not happen due to these removals.
+    */
+  def removeSinglePreferences(pref: UsersPreferences, singlePrefs: Preferences): UsersPreferences = {
     val optionsTaken = singlePrefs.map(x=>x._1)
     val filtered = pref.filter(_.length>1)
     filtered.map(x => x.filter(y => !optionsTaken.contains(y._1))).filter(!_.isEmpty)
   }
 
+  /**
+    * This function removes recursively single preferences from the customer matrix, and
+    * returns that simplified matrix and the partial solution found so far. Every time we
+    * remove a single preference that preference will be part of the solution. In case
+    * there is no solution it returns None.
+    * After this function is called it can happen 2 different scenarios:
+    * - we can have solved the whole problem cause the matrix is completely simplified.
+    * - we can have found part of the solution...but we still have a simplified matrix with
+    * some different solutions possible. We have to take a decision about which one is the best.
+    *
+    */
   def simplifyMatrix(pref: UsersPreferences): Option[(Preferences,UsersPreferences)] = {
     pref.filter(_.length==1).flatten match {
       case Nil => Some(Nil,pref)
       case xs => {
-        if(!isPossible(xs)) None else {
-          val simplifiedPref = removePreferences(pref, xs)
+        if(!existsSolution(xs)) None else {
+          val simplifiedPref = removeSinglePreferences(pref, xs)
           simplifyMatrix(simplifiedPref) match {
             case Some(y) => Some((xs.distinct:::y._1, y._2))
             case None => None
@@ -36,16 +57,30 @@ object Solver {
     }
   }
 
-  def combinationList[T](ls:List[List[T]]): List[List[T]] = ls match {
+  /**
+    * It receives the matrix of preferences (simplified) and returns the
+    * list of all the possible solutions, valid or not.
+    */
+  def generateSolutionCombinations[T](ls:List[List[T]]): List[List[T]] = ls match {
     case Nil => Nil::Nil
-    case head :: tail => val rec = combinationList[T](tail)
+    case head :: tail => val rec = generateSolutionCombinations[T](tail)
       rec.flatMap(r => head.map(t => t::r))
   }
 
+  /**
+    * This function checks whether a solution is valid or not. It is not valid
+    * if there are conflicts. For example, this solution is not valid:
+    * ((1 M)(2 G)(1 G)) because the color 1 can not be produced
+    * in M and G at the same time.
+    */
   def isValidSolution(solution: Preferences): Boolean = {
     solution.length == solution.groupBy(_._1).toList.length
   }
 
+  /**
+    * It returns the best solution from the list of all possible valid solutions. This
+    * is the one with the maximum number of gloss colors (which are cheaper to generate).
+    */
   def getOptimalSolution(validSolutions: List[Preferences]): Preferences ={
     validSolutions.sortWith((l,r) => countNumberOfGlossColours(l)>countNumberOfGlossColours(r)).head
   }
@@ -57,6 +92,10 @@ object Solver {
     }
   }
 
+  /**
+    * In case there are missing colors in the final solution we have
+    * to fill it with the matte version of those colors.
+    */
   def fillMissingColors(solution: Preferences, numColors:Int): Preferences ={
     val missingColors =
       for{ i<- 1 to numColors
@@ -70,7 +109,7 @@ object Solver {
     simplifyMatrix(userPrefs) match {
       case None => None
       case Some(simplified) => {
-        val comb = combinationList(simplified._2)
+        val comb = generateSolutionCombinations(simplified._2)
         val validCombs = comb.filter(x => isValidSolution(x))
         val optimal = getOptimalSolution(validCombs)
         Some(fillMissingColors(optimal:::simplified._1, numColors))
--- src/test/scala/SolverTest.scala
+++ src/test/scala/SolverTest.scala
@@ -9,21 +9,21 @@ class SolverTest extends FunSuite {
 
   test("test of isPossible(...) method"){
     val pref = List((1,'M),(2,'G),(3,'M))
-    assert(isPossible(pref)==true)
+    assert(existsSolution(pref)==true)
     val pref2 = List((1,'M),(1,'G),(3,'M))
-    assert(isPossible(pref2)==false)
+    assert(existsSolution(pref2)==false)
     val pref3 = List((1,'M),(2,'M),(3,'M))
-    assert(isPossible(pref3)==true)
+    assert(existsSolution(pref3)==true)
   }
 
   test("test of removePreferences(...) method"){
     val userPrefs = List( List((1,'M),(2,'M)), List((1,'G)), List((3,'M),(2,'G)) )
     val singlePrefs = List( (1,'M), (1,'G), (2,'G) )
-    assert(removePreferences(userPrefs, singlePrefs) == List(List((3,'M))))
+    assert(removeSinglePreferences(userPrefs, singlePrefs) == List(List((3,'M))))
 
     val userPrefs2 = List( List((1,'M),(2,'M)), List((3,'M),(2,'G)) )
     val singlePrefs2 = List( (1,'G), (2,'G) )
-    assert(removePreferences(userPrefs2, singlePrefs2) == List(List((3,'M))))
+    assert(removeSinglePreferences(userPrefs2, singlePrefs2) == List(List((3,'M))))
   }
 
   test("test of simplifyMatrix(...)"){
@@ -43,7 +43,7 @@ class SolverTest extends FunSuite {
 
   test("test of the combinationList generator"){
     val pref = List( List( (1,'M),(2,'M) ), List( (1,'M), (2,'G) ) )
-    assert(combinationList(pref) == List( List( (1,'M),(1,'M) ), List( (2,'M),(1,'M) ),
+    assert(generateSolutionCombinations(pref) == List( List( (1,'M),(1,'M) ), List( (2,'M),(1,'M) ),
       List( (1,'M),(2,'G) ), List( (2,'M),(2,'G) )) )
   }
 
